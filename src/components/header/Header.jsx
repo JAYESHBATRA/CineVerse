723a969a33c14cf13502ffda3b3464202986bc56
JAYESHBATRA <112322302+JAYESHBATRA@users.noreply.github.com>
2023-04-08 23:59:05 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/cineverse-logo.png";
+import logo from "../../assets/Cineverse-logo.png";
 
 const Header = () => {
     const [show, setShow] = useState("top");
